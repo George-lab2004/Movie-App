daa25dd604bf44ebee3a0629b1fefd6af71b5385
George <George.milad2@gmail.com>
2024-11-02 16:04:04 +0200

    4th commit

--- src/Components/SignUp/Signup.jsx
+++ src/Components/SignUp/Signup.jsx
@@ -262,7 +262,7 @@ export default function Register() {
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 Already have an account?{" "}
                 <Link
-                  href="/login"
+                  to="/login"
                   className="font-medium text-blue-600 hover:underline dark:text-blue-500"
                 >
                   Login here
